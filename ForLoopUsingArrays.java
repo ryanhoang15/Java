c9aace86c6e7502d9857030921f2d4c178b00fe2
ryanhoang15 <61032642+ryanhoang15@users.noreply.github.com>
2020-02-17 14:47:21 -0800

    Update ForLoopUsingArrays.java

--- ForLoopUsingArrays.java
+++ ForLoopUsingArrays.java
@@ -6,9 +6,7 @@ import java.util.Arrays;
 
 public class ForLoopUsingArrays {
     public static void main(String[] args) {
-        //   for (int i = 0; i < 28; i++) {
-        // 		System.out.println("Congrats on finishing Java loops!");
-        // 	  }
+        
         int[] money = {1,2,3,4,5};
         int total = 0;
         for (int i = 0; i < money.length; i++) {
