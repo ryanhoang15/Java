494dd887634abc5510a3780d1fe841968d544f78
ryanhoang15 <61032642+ryanhoang15@users.noreply.github.com>
2020-02-23 13:43:04 -0800

    Update ForLoopUsingArrays.java

--- ForLoopUsingArrays.java
+++ ForLoopUsingArrays.java
@@ -9,10 +9,16 @@ public class ForLoopUsingArrays {
         
         int[] money = {1,2,3,4,5};
         int total = 0;
+        
+        // this loop is looping through the array and adding the index value to total
         for (int i = 0; i < money.length; i++) {
             total = total + money[i];
         }
+        
         System.out.println(total);
+        
+        // this is printing out the array using the Arrays.tostring method b/c if u don't use it than 
+        // it will print out the memory location
         System.out.println(Arrays.toString(money));
     }
 
