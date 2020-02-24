906e6c400f2e42f1f60950f1710a05685a9887d8
ryanhoang15 <61032642+ryanhoang15@users.noreply.github.com>
2020-02-24 08:38:01 -0800

    Update CalculateTotalWithArrayList.java

--- CalculateTotalWithArrayList.java
+++ CalculateTotalWithArrayList.java
@@ -3,8 +3,11 @@ package ForLoopsEx;
 import java.util.ArrayList;
 
 public class CalculateTotalWithArrayList {
+    
     public static void main(String[] args) {
+        //this creates an ArrayList
         ArrayList<Double> expenses = new ArrayList<Double>();
+        //.add() method is adding to the ArrayList
         expenses.add(74.46);
         expenses.add(63.99);
         expenses.add(10.57);
@@ -14,6 +17,7 @@ public class CalculateTotalWithArrayList {
 
         // Iterate over expenses
         for (int i = 0; i < expenses.size(); i++) {
+            // .get() method is used for ArrayList and is getting value at the index location and adding it to total
             total = total + expenses.get(i);
         }
 
