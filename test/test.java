0a4da067f40d3189c36b56889748aa6e3836835b
ryanhoang15 <61032642+ryanhoang15@users.noreply.github.com>
2020-03-12 15:11:51 -0700

    Add files via upload

---
+++ test/test.java
@@ -0,0 +1,32 @@
+package test;
+
+import java.util.Scanner;
+
+public class test {
+
+    //import java.lang.Math; // this is use for doing math
+    public static void main(String[] arg) {
+        //Scanner sc = new Scanner(System.in);
+        //String name;
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(i);
+            // }
+            // do {
+            //   System.out.println("Enter your name: ");
+            //   name = sc.nextLine();
+            //} while (!name.equals("")); //value
+
+            // }while(name == " ") //memory location
+
+
+            // }
+
+            // byte a = 1;  // 00000001
+            // byte b = 2;  // 00000010
+            //  System.out.println("a and b " + (a & b)); // 00000000
+            //  System.out.println("a or b " + (a | b)); // 00000011
+            //  System.out.println("a xor b " + (a ^ b)); // 00000011
+            //  System.out.println("not a " + (~a)); // 1111 1110
+        }
+    }
+}
