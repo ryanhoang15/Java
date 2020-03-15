a5cf7342342321d2fc25d73029c3f410cbb3abfd
ryanhoang15 <61032642+ryanhoang15@users.noreply.github.com>
2020-03-15 14:22:48 -0700

    Add files via upload

---
+++ test/DemoMethodTest.java
@@ -0,0 +1,11 @@
+package test;
+
+public class DemoMethodTest {
+    int n1 = 8;
+    int n2 = 12;
+    String st = "hi there";
+
+    public int addnum(int n1, int n2){
+        return n1 + n2;
+    }
+}
