3162fa8918f99f8c3d663e0c8b1e9d41b30579e6
ryanhoang15 <61032642+ryanhoang15@users.noreply.github.com>
2020-04-12 21:28:53 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+package Javapt1;
+
+
+
+public class HelloWorld {
+    public static void main(String[] arg) {
+        System.out.println("Hello World");
+        int width = 20;
+        //System.out.println(width + 20);
+        width = 30;
+        System.out.println(width + width);
+//        int num = 24;
+//        int num2 = 20;
+//        int sum = add(num,num2);
+//        System.out.println(sum);
+
+    }
+//    public static int add(int num, int num2){
+//        int total = num + num2;
+//        return total;
+//    }
+}
+
