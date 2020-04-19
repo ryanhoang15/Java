ff3d1b5edb434a9ab5d94bfa1ef1af30b93de73b
ryanhoang15 <61032642+ryanhoang15@users.noreply.github.com>
2020-04-19 00:11:28 -0700

    Add files via upload

---
+++ coffee.java
@@ -0,0 +1,14 @@
+package ForLoopsEx;
+
+public class coffee {
+    public static void main(String[] args) {
+
+
+        for (int cupsOfCoffee = 1; cupsOfCoffee <= 100; cupsOfCoffee++) {
+            System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+        }
+
+    }
+
+}
+
